dae9b2697a341a9c4382823f33318c03edbdcf1c
Evgeniy Berendyaev <evgeniy.berendyaev@gmail.com>
2019-12-23 23:39:33 +0300

    Added all necessary files

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -2,6 +2,6 @@
 wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 apt-get update
-apt install -y mongodb-org
+apt-get install -y mongodb-org
 systemctl start mongod
 systemctl enable mongod
