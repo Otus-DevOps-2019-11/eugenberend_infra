4ec8d2d1b1c4ec81889f807aee21d466ef5d4777
Evgeniy Berendyaev <Evgeniy.Berendyaev@gmail.com>
2019-12-24 11:25:20 +0300

    Properly prepared jsons for main task

---
+++ config-scripts/create-reddit-vm.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+gcloud compute --project=infra-262515 \
+	instances create reddit-vm \
+	--zone=europe-north1-a \
+	--machine-type=f1-micro \
+	--image=reddit-base-1577174161 \
+	--tags puma-server
