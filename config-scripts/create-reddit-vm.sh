930c151af35ae7e5e4265b1db2d259b732fdcc6b
Evgeniy Berendyaev <Evgeniy.Berendyaev@gmail.com>
2019-12-24 15:13:39 +0300

    Modify gcloud call

--- config-scripts/create-reddit-vm.sh
+++ config-scripts/create-reddit-vm.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-gcloud compute --project=infra-262515 \
-	instances create reddit-vm \
+gcloud compute instances create reddit-vm \
 	--zone=europe-north1-a \
 	--machine-type=f1-micro \
-	--image=reddit-base-1577188522 \
+	--image-family reddit-full \
 	--tags puma-server
