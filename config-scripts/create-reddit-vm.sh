e28138b52f400d5efe5631d2a29f7fd1002d10bf
Evgeniy Berendyaev <Evgeniy.Berendyaev@gmail.com>
2019-12-24 13:47:32 +0300

    Prepare to run immutable VM

--- config-scripts/create-reddit-vm.sh
+++ config-scripts/create-reddit-vm.sh
@@ -3,5 +3,5 @@ gcloud compute --project=infra-262515 \
 	instances create reddit-vm \
 	--zone=europe-north1-a \
 	--machine-type=f1-micro \
-	--image=reddit-base-1577174161 \
+	--image=reddit-base-1577182650 \
 	--tags puma-server
