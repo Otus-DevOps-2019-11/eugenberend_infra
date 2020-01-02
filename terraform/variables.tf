fe9ec8fe2806e3b5f0238d3634effb4be6c82aa6
Evgeniy Berendyaev <evgeniy.berendyaev@gmail.com>
2020-01-02 23:39:52 +0300

    Move to google cloud shell

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,9 +10,6 @@ variable public_key_path {
   # Описание переменной
   description = "Path to the public key used for ssh access"
 }
-variable disk_image {
-  description = "Disk image"
-}
 variable private_key_path {
   description = "Path to the private key used for ssh access"
 }
